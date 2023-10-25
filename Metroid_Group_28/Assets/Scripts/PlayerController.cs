0f0bcb314842f688679c1f9d264cefe58606c79d
IsaacSoto2021 <iisoto@csuchico.edu>
2023-10-24 17:40:57 -0700

    Added Complete UI for Menu and Death screen , aswell set up  Scripts for UI and Player Controller

---
+++ Metroid_Group_28/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// IsaacSoto
+/// 10/24/23
+/// [PLayer Controller for movement and player abilities]
+
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Metroid_Group_28/Assets/Scripts/UIManagement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+/// Isaac Soto
+/// 10/24/2023
+/// [ ALL UI MANAGEMENT of LIVES and Text]
+
+
+public class UIManagement : MonoBehaviour
+{
+   public PlayerController PlayerController;
+
+    public TMP_Text livesText;
+    // Update is called once per frame
+
+    void Update()
+    {
+        // Sets the text string as "Lives: " (A String) Plus Total Score
+        //livesText.text = "Lives: " + PlayerController.lives.Tostring();
+    }
+}
