cc801c9a457d98999722dc5641ba857db7df05f3
IsaacSoto2021 <iisoto@csuchico.edu>
2023-10-31 14:57:12 -0700

    fixes and changes

--- Metroid_Group_28/Assets/Scripts/DeathFloor.cs
+++ Metroid_Group_28/Assets/Scripts/DeathFloor.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DeathFloor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
         
+      //  if (other.gameObject.tag == "Player")
+        {
+           // PlayerController Samus = other.gameObject.GetComponent<PlayerController>();
+           // other.gameObject.transform.position = Samus.startPos;
+        }
+
+
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
 }
--- Metroid_Group_28/Assets/Scripts/Portal.cs
+++ Metroid_Group_28/Assets/Scripts/Portal.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Isaac soto
+/// 10/31/2023
+/// [
+/// </summary>
+
 public class Portal : MonoBehaviour
 {
 
